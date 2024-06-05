0dd64475daeba230fb2e1c002d80d4b8ca50b0e0
Miguel Filipe <miguel.filipe@gmail.com>
2024-06-05 12:27:17 +0100

    start working on the test

--- ingester/mainloop_test.go
+++ ingester/mainloop_test.go
@@ -3,13 +3,33 @@ package ingester_test
 import "testing"
 
 func TestBlockConsumptionLoop(t *testing.T) {
-	t.Skip("not implemented")
+	testcases := []string{
+		"we're very behind, trying to catch up",
+		"we're up to date, following the head",
+		"we're erroring systematically, the RPC node is broken, all API calls are failing",
+		"we're erroring only on GetBlockByNumber, a specific jsonRPC on the RPC node is broken",
+	}
+
+	for _, testcase := range testcases {
+		t.Run(testcase, func(t *testing.T) {
+			t.Skip("not implemented")
+		})
+	}
 }
 
 func TestBlockSendingLoop(t *testing.T) {
-	t.Skip("not implemented")
+	testcases := []string{
+		"we're up to date, following the head",
+		"we're failing intermittently, the Dune API is broken",
+		"we're erroring systematically, the Dune API is down",
+	}
+	for _, testcase := range testcases {
+		t.Run(testcase, func(t *testing.T) {
+			t.Skip("not implemented")
+		})
+	}
 }
 
-func TestRunLoop(t *testing.T) {
+func TestRunLoopHappyCase(t *testing.T) {
 	t.Skip("not implemented")
 }
